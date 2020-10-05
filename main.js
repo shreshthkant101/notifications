a60af47b7a5781afd24671e7177c2286c6d7d9ad
Shreshth kant <63898444+shreshthkant101@users.noreply.github.com>
2020-10-06 01:09:59 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function sendd() {
     var options = {
         body: 'Hello!'
     }
-    var n = new Notification('My First notification', options);
+    var n = new window.Notification('My First notification', options);
     n.onshow = function(helo) {
         alert("sent");
     }
